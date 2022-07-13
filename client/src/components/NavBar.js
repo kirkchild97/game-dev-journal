0bd486aa43e37fa675cfdd84e23b460c9ad6d6c3
Kirkland Childers <robert.kirkland.childers@gmail.com>
2022-07-13 18:10:54 -0400

    Visual Changes made to Login and Register Page and Navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,7 +1,17 @@
-import { NavLink } from "react-router-dom";
+import {
+    NavLink,
+    Link,
+    useLocation,
+    matchPath
+} from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 
 import { logout } from "../state/reducer/userReducer";
+
+import {
+    Tabs,
+    Tab
+} from '@mui/material'
 
 const NavBar = () => {
     const { isLoggedIn, homeUserName } = useSelector(state => state.user);
@@ -13,22 +23,56 @@ const NavBar = () => {
         await dispatch(logout());
     }
 
+    const giveRoutes = () => [
+        isLoggedIn ? `user/:userName` : '',
+        `user/:userName/game/new`
+    ]
+
+    const useRouteMatch = (patterns) => {
+        const { pathname } = useLocation();
+        for (let i = 0; i < patterns.length; i += 1) {
+            const pattern = patterns[i];
+            const possibleMatch = matchPath(pattern, pathname);
+            if (possibleMatch !== null) {
+                return possibleMatch;
+            }
+        }
+        return null;
+    }
+
+    // return (
+    //     <nav className="row">
+    //         <div className="col">
+    //             <NavLink to={isLoggedIn ? `user/${homeUserName}` : ''} end className='btn btn-success'>Home</NavLink>
+    //             {
+    //                 isLoggedIn ? 
+    //                 <>
+    //                     <NavLink to={`user/${homeUserName}/new`} end className='btn btn-success'>Create New Game Idea</NavLink>
+    //                 </> : ''
+    //             }
+    //         </div>
+    //         {isLoggedIn ? 
+    //             <div className="col-2">
+    //                 <button onClick={(e) => handleLogout(e)} className='btn btn-danger float-end'>Logout</button>
+    //             </div> : ''}
+    //     </nav>
+    // );
+    const routeMatch = useRouteMatch(giveRoutes());
+    const currentTab = routeMatch?.pattern?.path;
+
     return (
-        <nav className="row">
+        <Tabs value={currentTab}>
             <div className="col">
-                <NavLink to={isLoggedIn ? `user/${homeUserName}` : ''} end className='btn btn-success'>Home</NavLink>
+                <Tab label='Home' value={isLoggedIn ? `user/:userName` : ''} to={`user/${homeUserName}`} component={Link}/>
                 {
                     isLoggedIn ? 
                     <>
-                        <NavLink to={`user/${homeUserName}/new`} end className='btn btn-success'>Create New Game Idea</NavLink>
+                        <Tab label='Create New Game Idea' value={`user/:userName/new`} to={`user/${homeUserName}/new`} component={Link} />
                     </> : ''
                 }
             </div>
-            {isLoggedIn ? 
-                <div className="col-2">
-                    <button onClick={(e) => handleLogout(e)} className='btn btn-danger float-end'>Logout</button>
-                </div> : ''}
-        </nav>
+            {isLoggedIn ? <Tab className="col-2" label='Logout' onClick={(e) => handleLogout(e)} /> : ''}
+        </Tabs>
     );
 }
 
